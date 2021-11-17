6a648991e343cfe872b1b00431bcae6627f5800e
genejackett <genejackett@gmail.com>
2021-11-17 01:29:56 -0600

    Including algorithm method: Knapsack   Still needs adjustments.   Not sure what to do.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,32 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Main {
+    /*public class knapsack{
+        static int max(int a, int b){
+            return(a>b) ? a : b;
+        }
+        static int knapSack(int W, int cost[], int value, int n){
+            int i,w;
+            int K[][] = new int[n+1][W+1];
+            //Build table K in bottom up manner
+            for(i=0;i<=n; i++){
+                for (w=0;w<=W; w++){
+                    if(i == 0 || w ==0){
+                        K[i][w] = 0;
+                    }
+                    else if(cost[i-1]<){
+                        K[i][w] = max();//not sure
+                    }
+                    else{
+                        K[i][w] = K[i];//not sure
+                    }
+                }
+            }
+            return K[n][W];
+        }
+    }
+
+     */
 
     public static void main(String[] args) {
 	// write your code here
@@ -33,7 +59,7 @@ public class Main {
                     attributes[2] = attributes[j + 1];
                 }
                 System.out.println(Arrays.toString(attributes));
-
+                //make call to function
 
             }
         } catch (IOException e) {
